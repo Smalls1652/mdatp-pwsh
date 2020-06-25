6c2181deff100766e7e871249267a1f655f83d57
Smalls1652 <smalls.online@icloud.com>
2020-06-25 11:10:37 -0400

    Changed the build script

--- BuildModule.ps1
+++ BuildModule.ps1
@@ -35,9 +35,9 @@ $ConfigSplat = @{
 
 Push-Location -Path "./src/"
 
-dotnet clean
-
-dotnet publish /property:PublishWithAspNetCoreTargetManifest=false
+Start-Process -FilePath "dotnet" -ArgumentList @("clean") -Wait
+Start-Sleep -Seconds 5
+Start-Process -FilePath "dotnet" -ArgumentList @("publish", "/property:PublishWithAspNetCoreTargetManifest=false") -Wait
 
 Pop-Location
 
@@ -45,12 +45,12 @@ if (Test-Path -Path "./build-out") {
     Remove-Item -Path "./build-out" -Recurse -Force
 }
 
-$null = New-Item -Type Directory -Path "./build-out"
-$null = New-Item -Type Directory -Path "./build-out/mdatp-pwsh"
+$null = New-Item -Type Directory -Path "./build"
+$null = New-Item -Type Directory -Path "./build/mdatp-pwsh"
 
-Copy-Item -Path "./src/bin/Debug/netstandard2.0/publish/mdatp_pwsh.dll" -Destination "./build-out/mdatp-pwsh/"
-Copy-Item -Path "./src/bin/Debug/netstandard2.0/publish/Microsoft.Identity.Client.dll" -Destination "./build-out/mdatp-pwsh/"
-Copy-Item -Path "./src/bin/Debug/netstandard2.0/publish/Newtonsoft.Json.dll" -Destination "./build-out/mdatp-pwsh/"
-Copy-Item -Path "./license.txt" -Destination "./build-out/mdatp-pwsh/License.txt"
+Copy-Item -Path "./src/bin/Debug/netstandard2.0/publish/mdatp_pwsh.dll" -Destination "./build/mdatp-pwsh/"
+Copy-Item -Path "./src/bin/Debug/netstandard2.0/publish/Microsoft.Identity.Client.dll" -Destination "./build/mdatp-pwsh/"
+Copy-Item -Path "./src/bin/Debug/netstandard2.0/publish/Newtonsoft.Json.dll" -Destination "./build/mdatp-pwsh/"
+Copy-Item -Path "./license.txt" -Destination "./build/mdatp-pwsh/License.txt"
 
 New-ModuleManifest @ConfigSplat
