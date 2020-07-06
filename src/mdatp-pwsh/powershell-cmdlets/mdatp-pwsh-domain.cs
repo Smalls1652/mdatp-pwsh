b89eaec44f335bbe1afb12e7bbb30ca8924dfbad
Smalls1652 <smalls.online@icloud.com>
2020-07-06 12:20:24 -0400

    Changed classes to match new names

---
+++ src/mdatp-pwsh/powershell-cmdlets/mdatp-pwsh-domain.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Management.Automation;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Identity.Client;
+using mdatp_pwsh.Classes;
+
+namespace mdatp_pwsh
+{
+    [Cmdlet(VerbsCommon.Get, "DatpDomainStats")]
+    public class GetDatpDomainStats : PSCmdlet
+    {
+        [Parameter(Position = 0)]
+        public string DomainName
+        {
+            get { return domainName; }
+            set { domainName = value; }
+        }
+        private static string domainName;
+
+        private static string apiUri;
+
+        protected override void BeginProcessing()
+        {
+            if ((AuthenticationResult)SessionState.PSVariable.GetValue("DatpGraphToken") == null)
+            {
+                throw new Exception("Graph token not found.");
+            }
+
+            apiUri = $"/domains/{domainName}/stats";
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteVerbose("Getting token from session.");
+            AuthenticationResult token = (AuthenticationResult)SessionState.PSVariable.GetValue("DatpGraphToken");
+
+            WriteVerbose("Starting api call.");
+            HttpResponseMessage apiResponse = new ApiCaller().MakeGetApiCall(apiUri, token);
+
+            string apiJson = apiResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+            WriteVerbose(apiJson);
+
+            DomainStats apiResult = JsonConvert.DeserializeObject<DomainStats>(apiJson);
+
+            WriteObject(apiResult);
+
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Get, "DatpDomainRelated")]
+    public class GetDatpDomainRelated : PSCmdlet
+    {
+        [Parameter(Position = 0)]
+        public string DomainName
+        {
+            get { return domainName; }
+            set { domainName = value; }
+        }
+        private static string domainName;
+
+        [Parameter(Position = 1)]
+        [ValidateSet(
+            "Alerts",
+            "Machines"
+        )]
+        public string Type
+        {
+            get { return searchType; }
+            set { searchType = value; }
+        }
+        private static string searchType = "Machines";
+
+        private static string apiUri;
+
+
+        protected override void BeginProcessing()
+        {
+            if ((AuthenticationResult)SessionState.PSVariable.GetValue("DatpGraphToken") == null)
+            {
+                throw new Exception("Graph token not found.");
+            }
+
+            apiUri = $"/domains/{domainName}/{searchType.ToLower()}";
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteVerbose("Getting token from session.");
+            AuthenticationResult token = (AuthenticationResult)SessionState.PSVariable.GetValue("DatpGraphToken");
+
+            WriteVerbose("Starting api call.");
+            HttpResponseMessage apiResponse = new ApiCaller().MakeGetApiCall(apiUri, token);
+
+            string apiJson = apiResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+            WriteVerbose(apiJson);
+
+            dynamic apiResult = null;
+
+            switch (searchType)
+            {
+                case "Alerts":
+                    apiResult = JsonConvert.DeserializeObject<AlertCollection>(apiJson);
+                    break;
+
+                case "Machines":
+                    apiResult = JsonConvert.DeserializeObject<MachineCollection>(apiJson);
+                    break;
+
+            }
+
+            foreach (dynamic obj in apiResult.value)
+            {
+                WriteObject(obj);
+            }
+
+        }
+    }
+
+}
---
+++ src/mdatp-pwsh/powershell-cmdlets/mdatp-pwsh-user.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Identity.Client;
+using mdatp_pwsh.Classes;
+
+namespace mdatp_pwsh
+{
+    [Cmdlet(VerbsCommon.Get, "DatpUserMachines")]
+    public class GetDatpUserMachines : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        private static string userName;
+
+        private static string apiUri;
+
+        protected override void BeginProcessing()
+        {
+            if ((AuthenticationResult)SessionState.PSVariable.GetValue("DatpGraphToken") == null)
+            {
+                throw new Exception("Graph token not found.");
+            }
+
+            apiUri = $"/users/{userName}/machines";
+
+            WriteVerbose($"Getting machines '{userName}' has logged into.");
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteVerbose("Getting token from session.");
+            AuthenticationResult token = (AuthenticationResult)SessionState.PSVariable.GetValue("DatpGraphToken");
+
+            WriteVerbose("Starting api call.");
+            HttpResponseMessage apiResponse = new ApiCaller().MakeGetApiCall(apiUri, token);
+
+            string apiJson = apiResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+            WriteVerbose(apiJson);
+
+            MachineCollection apiResult = JsonConvert.DeserializeObject<MachineCollection>(apiJson);
+
+            foreach (Machine item in apiResult.value)
+            {
+                WriteObject(item);
+            }
+
+        }
+
+        protected override void EndProcessing()
+        {
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Get, "DatpUserAlerts")]
+    public class GetDatpUserAlerts : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        private static string userName;
+
+        private static string apiUri;
+
+        protected override void BeginProcessing()
+        {
+            if ((AuthenticationResult)SessionState.PSVariable.GetValue("DatpGraphToken") == null)
+            {
+                throw new Exception("Graph token not found.");
+            }
+
+            apiUri = $"/users/{userName}/alerts";
+
+            WriteVerbose($"Getting alerts triggered by '{userName}'.");
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteVerbose("Getting token from session.");
+            AuthenticationResult token = (AuthenticationResult)SessionState.PSVariable.GetValue("DatpGraphToken");
+
+            WriteVerbose("Starting api call.");
+            HttpResponseMessage apiResponse = new ApiCaller().MakeGetApiCall(apiUri, token);
+
+            string apiJson = apiResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+            WriteVerbose(apiJson);
+
+            AlertCollection apiResult = JsonConvert.DeserializeObject<AlertCollection>(apiJson);
+
+            foreach (Alert item in apiResult.value)
+            {
+                WriteObject(item);
+            }
+
+        }
+
+        protected override void EndProcessing()
+        {
+        }
+    }
+}
