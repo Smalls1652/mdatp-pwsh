ddbc931d61e63b4022526752c5d02402a826ae47
Smalls1652 <smalls.online@icloud.com>
2020-07-06 15:59:00 -0400

    Added error handler

---
+++ src/mdatp-pwsh/helpers/mdatp-error-handling.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+using MdatpPwsh.Classes;
+
+namespace MdatpPwsh
+{
+    [Serializable]
+    public class DatpException : Exception
+    {
+        public ErrorMessage DatpError { get; set; }
+
+        public DatpException(string message) : base(message) { }
+        public DatpException(string message, System.Exception inner) : base(message, inner) { }
+        public DatpException(string message, ErrorMessage datpError) : this(message)
+        {
+            DatpError = datpError;
+        }
+    }
+
+    public class ErrorHandler
+    {
+        private ErrorMessage ConvertErrorToClass(HttpContent content)
+        {
+            ErrorMessage errorResponse = JsonConvert.DeserializeObject<ErrorMessage>(content.ReadAsStringAsync().GetAwaiter().GetResult());
+
+            return errorResponse;
+        }
+
+        private bool IsError(HttpStatusCode statusCode)
+        {
+            bool eval;
+
+            switch (statusCode)
+            {
+                case HttpStatusCode.BadRequest:
+                case HttpStatusCode.Unauthorized:
+                case HttpStatusCode.Forbidden:
+                case HttpStatusCode.NotFound:
+                case HttpStatusCode.InternalServerError:
+                    eval = true;
+                    break;
+
+                default:
+                    eval = false;
+                    break;
+            }
+
+            return eval;
+        }
+
+        public void ParseApiResponse(HttpResponseMessage response)
+        {
+            bool rspIsError = IsError(response.StatusCode);
+
+            switch (rspIsError)
+            {
+                case true:
+                    ErrorMessage errorMsg = ConvertErrorToClass(response.Content);
+                    throw new DatpException(errorMsg.error.message, errorMsg);
+
+                default:
+                    break;
+
+            }
+        }
+    }
+}
