015ec64b7ac51baf68eb53c3e05227e35ece3849
Smalls1652 <smalls.online@icloud.com>
2020-07-26 17:26:26 -0400

    Updated build script
    - Made the module manifest a static file to update.

--- BuildModule.ps1
+++ BuildModule.ps1
@@ -3,43 +3,6 @@ param(
 
 )
 
-$ConfigSplat = @{
-    "Path"               = "./build/mdatp-pwsh/mdatp-pwsh.psd1";
-    "RootModule"         = "MdatpPwsh.dll";
-    "Guid"               = [guid]"afc0e191-ffe7-4261-ba9e-d59652423d8c";
-    "Description"        = "MDATP PowerShell Module";
-    "Author"             = "Tim Small";
-    "CompanyName"        = "Smalls.Online";
-    "Copyright"          = 2019;
-    "ModuleVersion"      = "2020.7.1";
-    "Prerelease"         = "alpha2";
-    "ProjectUri"         = "https://github.com/smalls1652/mdatp-pwsh";
-    "LicenseUri"         = "https://raw.githubusercontent.com/Smalls1652/mdatp-pwsh/master/license.txt";
-    "CmdletsToExport"    = @(
-        "Set-DatpModuleConfig",
-        "Connect-DatpGraph",
-        "Get-DatpMachine",
-        "Get-DatpMachineAlerts",
-        "Get-DatpMachineUsers",
-        "Set-DatpMachineIsolation",
-        "Add-DatpMachineTag",
-        "Remove-DatpMachineTag",
-        "Start-DatpMachineScan",
-        "Get-DatpMachineAction",
-        "Start-DatpInvestigationPkgCollection",
-        "Get-DatpDomainStats",
-        "Get-DatpDomainRelated",
-        "Get-DatpUserMachines",
-        "Get-DatpUserAlerts",
-        "Get-DatpAlert",
-        "Update-DatpAlert"
-    );
-    "RequiredAssemblies" = @(
-        "Microsoft.Identity.Client.dll",
-        "Newtonsoft.Json.dll"
-    )
-}
-
 Push-Location -Path "./src/"
 
 try {
@@ -62,4 +25,4 @@ Copy-Item -Path "./src/bin/Debug/netstandard2.1/publish/Microsoft.Identity.Clien
 Copy-Item -Path "./src/bin/Debug/netstandard2.1/publish/Newtonsoft.Json.dll" -Destination "./build/mdatp-pwsh/"
 Copy-Item -Path "./license.txt" -Destination "./build/mdatp-pwsh/License.txt"
 
-New-ModuleManifest @ConfigSplat
+Copy-Item -Path "./module-manifest/mdatp-pwsh.psd1" -Destination "./build/mdatp-pwsh/"
