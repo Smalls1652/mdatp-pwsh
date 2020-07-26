14700ef1242ae571d90371f66aa37cea5789cfbc
Smalls1652 <smalls.online@icloud.com>
2020-07-25 20:53:55 -0400

    Added Invoke-DatpPatchApiCall internal cmdlet.

---
+++ src/mdatp-pwsh/powershell-cmdlets/api/InvokeDatpPatchApiCall.cs
@@ -0,0 +1,62 @@
+using System.Management.Automation;
+using System.Net.Http;
+using Microsoft.Identity.Client;
+
+using MdatpPwsh.ApiHelper;
+
+namespace MdatpPwsh
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "DatpPatchApiCall")]
+    [CmdletBinding()]
+    public class InvokeDatpPatchApiCall : Cmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string Uri
+        {
+            get { return uri; }
+            set { uri = value; }
+        }
+        private static string uri;
+
+        [Parameter(Position = 1, Mandatory = true)]
+        public string PostBody
+        {
+            get { return postBody; }
+            set { postBody = value; }
+        }
+        private static string postBody;
+
+        [Parameter(Position = 1, Mandatory = true)]
+        public AuthenticationResult Token
+        {
+            get { return token; }
+            set { token = value; }
+        }
+        private static AuthenticationResult token;
+
+        protected override void ProcessRecord()
+        {
+            HttpResponseMessage apiResponse = null;
+
+            try
+            {
+                ApiCaller apiCaller = new ApiCaller(uri, postBody, token, HttpMethod.Patch);
+                apiResponse = apiCaller.MakeApiCall();
+                apiCaller.Close();
+            }
+            catch (DatpException e)
+            {
+                ThrowTerminatingError(
+                    new ErrorRecord(
+                        e,
+                        "Datp.ApiCall.Error",
+                        ErrorCategory.InvalidResult,
+                        e.DatpError.error
+                    )
+                );
+            }
+
+            WriteObject(apiResponse);
+        }
+    }
+}
