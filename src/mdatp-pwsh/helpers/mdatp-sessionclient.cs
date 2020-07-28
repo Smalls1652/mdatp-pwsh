baacca75a454d4820cec4d69b0759fa2402eb0bc
Smalls1652 <smalls.online@icloud.com>
2020-07-28 00:05:06 -0400

    Replaced ApiHelper with SessionClient
    - Created a new class that replaces ApiHelper and houses the HTTP
      client, Graph Token, and more.

---
+++ src/mdatp-pwsh/helpers/mdatp-sessionclient.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+using Microsoft.Identity.Client;
+
+namespace MdatpPwsh
+{
+    using Classes;
+    namespace Session
+    {
+        public class DatpSessionClient
+        {
+            public DatpSessionClient() { }
+
+            public DatpSessionClient(Uri baseApiUri, AuthenticationResult graphToken)
+            {
+                BaseApiUri = baseApiUri;
+                GraphToken = graphToken;
+
+                Connect();
+            }
+
+            public Uri BaseApiUri { get; set; }
+            public AuthenticationResult GraphToken { get; set; }
+            public HttpClient GraphClient { get; set; }
+
+            public void Connect()
+            {
+                GraphClient = new HttpClient();
+                GraphClient.BaseAddress = BaseApiUri;
+            }
+
+            public string SendApiCall(string endpoint, string apiPostBody, HttpMethod httpMethod)
+            {
+                HttpRequestMessage requestMessage = new HttpRequestMessage(httpMethod, endpoint);
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", GraphToken.AccessToken);
+
+                switch (String.IsNullOrEmpty(apiPostBody))
+                {
+                    case false:
+                        requestMessage.Content = new StringContent(apiPostBody);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                HttpResponseMessage responseMessage = GraphClient.SendAsync(requestMessage).GetAwaiter().GetResult();
+                new ErrorHandler().ParseApiResponse(responseMessage);
+                string responseMessageString = responseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                return responseMessageString;
+            }
+        }
+    }
+}
