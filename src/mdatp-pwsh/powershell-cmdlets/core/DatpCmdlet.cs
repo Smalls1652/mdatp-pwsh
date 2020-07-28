e2d640848bb8fb20e13c9ced9ee92a67e49d274b
Smalls1652 <smalls.online@icloud.com>
2020-07-28 00:07:15 -0400

    Added a DatpCmdlet abstract class
    - This abstract class inherits the PSCmdlet class. There are two
      functions in it to reduce the complexity of ensuring that the
      DatpSessionClient session state variable is easily passed and make it
      easier to change shared code.

---
+++ src/mdatp-pwsh/powershell-cmdlets/core/DatpCmdlet.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Management.Automation;
+using System.Net.Http;
+
+namespace MdatpPwsh
+{
+    using Session;
+    public abstract class DatpCmdlet : PSCmdlet
+    {
+        protected DatpSessionClient GetSessionClient()
+        {
+            DatpSessionClient sessionClient = (DatpSessionClient)SessionState.PSVariable.GetValue("DatpSessionClient");
+
+            if (null == sessionClient)
+            {
+                throw new Exception("DATP session client not found.");
+            }
+
+            return sessionClient;
+        }
+
+        protected string SendApiCall(string uri, string body, HttpMethod httpMethod)
+        {
+            DatpSessionClient sessionClient = GetSessionClient();
+
+            string apiResponse = sessionClient.SendApiCall(uri, body, httpMethod);
+
+            return apiResponse;
+        }
+    }
+}
